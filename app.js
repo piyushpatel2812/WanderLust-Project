07c124e3bb8e54ee946dd629a1be1dbc9cbdfd99
piyushpatel2812 <piyushpatel281205@gmail.com>
2026-02-05 13:41:42 +0530

    listing page

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const mongoose=require("mongoose");
 const Listing=require("./models/listing.js")
 const path =require("path");
 const methodOverride=require("method-override");
+const ejsMate = require("ejs-mate");
+const { render } = require("ejs");
+
 
 const MONGO_URL="mongodb://127.0.0.1:27017/wanderlust"
 // // call main fucntion 
@@ -24,6 +27,8 @@ app.set("views",path.join(__dirname,"views"));
 app.set("view engine","ejs");
 app.use(express.urlencoded({extended:true}));
 app.use(methodOverride("_method"));
+app.engine('ejs',ejsMate);
+app.use(express.static(path.join(__dirname,"/public")));
 
 
 app.get("/",(req,res)=>{// api 
